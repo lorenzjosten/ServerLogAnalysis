396e1eb334194990c384f718d0a1e375a1efe079
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 02:04:46 +0100

    Add destroy_old_file to input_files_controller

--- app/controllers/input_files_controller.rb
+++ app/controllers/input_files_controller.rb
@@ -1,4 +1,6 @@
 class InputFilesController < ApplicationController
+  before_action :destroy_old_file, only: [:create]
+
   def create
     @input_file = InputFile.new(input_file_params)
     if @input_file.save
@@ -10,5 +12,9 @@ class InputFilesController < ApplicationController
 
   def input_file_params
     params.require(:input_file).permit(:uploaded_file)
+  end
+
+  def destroy_old_file
+    InputFile.destroy_all
   end
 end
