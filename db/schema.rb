bdbb0d001354407dfd13fa957e592459f939aa6c
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 02:00:40 +0100

    Migrate AddAttributesToInputFile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302003801) do
+ActiveRecord::Schema.define(version: 20180302005912) do
 
   create_table "access_data", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20180302003801) do
   create_table "input_files", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "name"
+    t.string "content_type"
+    t.binary "data", limit: 10485760
   end
 
 end
