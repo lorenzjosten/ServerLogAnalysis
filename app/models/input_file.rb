1cdfa5a620123c5abb6070b773fe43853bcfb677
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 01:43:50 +0100

    Add uploaded_file to InputFile model

--- app/models/input_file.rb
+++ app/models/input_file.rb
@@ -1,3 +1,9 @@
 class InputFile < ApplicationRecord
   has_many :access_data
+
+  def uploaded_file=(file_field)
+    self.name = File.basename(file_field.original_filename).gsub(/[^\w._-]/,'')
+    self.content_type = file_field.content_type.chomp
+    self.data = file_field.read
+  end
 end
