c7c94ddad37238b5d210ee57b2daa434e8ab27c6
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 02:34:58 +0100

    Add set_input_file to AnalysisController

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,4 +1,19 @@
 class AnalysisController < ApplicationController
+
+  before_action :set_input_file, only: [:index]
+
   def index
+    byebug
+  end
+
+  private
+
+  def set_input_file
+    begin
+      @input_file = InputFile.find(params[:input_file][:id])
+    rescue
+      @input_file = InputFile.new
+      @input_file.access_data.build
+    end
   end
 end
