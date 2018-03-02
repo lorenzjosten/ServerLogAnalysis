7eeb10fdcd121d8ab4ea230d2d8f12f455a581e2
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 01:47:27 +0100

    Add create to input_file_controller

--- app/controllers/input_files_controller.rb
+++ app/controllers/input_files_controller.rb
@@ -1,2 +1,14 @@
 class InputFilesController < ApplicationController
+  def create
+    @input_file = InputFile.new(input_file_params)
+    if @input_file.save
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def input_file_params
+    params.require(:input_file).permit(:uploaded_file)
+  end
 end
