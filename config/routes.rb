246c7ed54bb8d6135d41ad6735744d6b0277fc82
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 02:41:21 +0100

    Add set_timeframe to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :access_data
   end
 
+  match 'timeframe', to: 'access_data#set_timeframe', via: [:put]
+
   root 'analysis#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
