4593b6504d511ca4e0ef0dfc37024125e9ba5f83
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 01:28:38 +0100

    Add nesting and root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :access_data
-  resources :input_files
+
+  get 'analysis/index'
+
+  resource :input_file, shallow: true do
+    resources :access_data
+  end
+
+  root 'analysis#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
