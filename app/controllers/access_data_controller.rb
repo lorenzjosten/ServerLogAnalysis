e67bd645ee5d64e4945e09c8e8c2c63c9e2ec724
lorenzjosten <lorenz.josten@gmail.com>
2018-03-02 02:28:55 +0100

    Rough implementation of setting timeframe

--- app/controllers/access_data_controller.rb
+++ app/controllers/access_data_controller.rb
@@ -1,2 +1,11 @@
 class AccessDataController < ApplicationController
+  def set_timeframe
+    timeframe(timeframe_params)
+  end
+
+  private
+
+  def timeframe_params
+    params.require(:timeframe).permit(:timeframe)
+  end
 end
--- app/models/access_datum.rb
+++ app/models/access_datum.rb
@@ -1,3 +1,7 @@
 class AccessDatum < ApplicationRecord
   belongs_to :input_file
+
+  def timeframe=(start_time_field, end_time_field)
+    @timeframe = {start_t: start_time_field, end_t: end_time_field}
+  end
 end
