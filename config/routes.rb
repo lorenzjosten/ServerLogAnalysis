eca1ca3803bcb0cd0ecedb5309ba6e23ffa8e05a
lorenzjosten <lorenz.josten@gmail.com>
2018-03-15 11:55:04 +0100

    Change routes, move views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resource :analysis
-  resource :timeframe
   resource :input_file, shallow: true do
     resources :access_data
   end
 
-  get 'analyses/index'
-  root 'analyses#index'
+  get 'access_data/index'
+  root 'access_data#index'
 end
