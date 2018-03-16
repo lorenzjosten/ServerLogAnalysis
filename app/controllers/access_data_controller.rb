5bfd667da768b7d6739c6dd0e22f1fc21da0aad4
lorenzjosten <lorenz.josten@gmail.com>
2018-03-16 15:26:53 +0100

    Extend AccessDataController

--- app/controllers/access_data_controller.rb
+++ app/controllers/access_data_controller.rb
@@ -1,2 +1,11 @@
 class AccessDataController < ApplicationController
+
+  def index
+    @access_data = AccessDatum.all
+  end
+
+  def show
+    @access_data = AccessDataum.url_filtered.stat_filtered.time_filtered(params[t1], params[t2])
+  end
+
 end
--- app/models/access_datum.rb
+++ app/models/access_datum.rb
@@ -1,3 +1,20 @@
 class AccessDatum < ApplicationRecord
   belongs_to :input_file
+
+  URL_FILTER_KEYS = ["%feed.atom", "%feed", "%wp-login.php", "/de", "/", "/en", "%/blog"] #blacklist urls
+  STAT_FILTER_KEYS = ["200"] #whitelist server response stats
+
+  scope :url_filtered, -> { URL_FILTER_KEYS.inject(all) {|filtered, url| filtered.merge(url_filter(url))} }
+  scope :stat_filtered, -> { STAT_FILTER_KEYS.inject(none) {|filtered, stat| filtered.or(stat_filter(stat))} }
+
+  scope :time_filter, -> (t1, t2) { where("access_time >= ? and access_time <= ?", t1, t2) }
+  scope :url_filter, -> (url) { where("not url like ?", url) }
+  scope :stat_filter, -> (stat) { where("stat like ?", stat) }
+
+  scope :start_time, -> { order('access_time asc').first.access_time }
+  scope :end_time, -> { order('access_time asc').last.access_time }
+  scope :urls, -> { select('url').distinct }
+  scope :blogs, -> { where('url like ?', '%blog%') }
+  scope :url_visits, -> (url) { select {|d| d.url == url }.count }
+  scope :url_visits_per_hour, -> (url, hour) { where('url like ?', url).select {|d| d.access_time.hour == hour}.count }
 end
